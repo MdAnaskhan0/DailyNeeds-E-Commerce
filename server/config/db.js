76ca8cd08f6dcc480859f3dc7ad74db4abb8222a
emon-needglobal <emon.needglobal@gmail.com>
2024-09-28 22:44:07 +0600

    succesfully connected mongodb database server

---
+++ server/config/db.js
@@ -0,0 +1,22 @@
+// config/db.js
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env
+dotenv.config();
+
+// MongoDB connection function
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,29 @@
-console.log("mongodb setup")
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+// Configure dotenv
+dotenv.config();
+
+//database connect
+
+connectDB();
+const app = express();
+
+//middleware
+
+app.use(morgan("dev"));
+app.use(express.json());
+// Routes
+app.get("/", (req, res) => {
+  res.send("emon");
+});
+
+// Define the port from environment variables or use 8000 as default
+const PORT = process.env.PORT || 8001;
+
+// Run the app
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
