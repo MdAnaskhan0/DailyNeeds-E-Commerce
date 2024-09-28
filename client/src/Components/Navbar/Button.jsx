ed34986e7a706cf5841a7c8873eb2f8a9d6350d7
John Doe <john.doe@example.com>
2024-09-29 02:08:50 +0600

    Working on Navbar with tailwind css style.

---
+++ client/src/Components/Navbar/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+    return (
+        <div>
+            <button className='bg-slate-500 text-bold rounded-sm text-white hover:bg-red-300 hover:text-black'>click</button>
+        </div>
+    )
+}
+
+export default Button
