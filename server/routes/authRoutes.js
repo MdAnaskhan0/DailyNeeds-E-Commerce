6580dce986c1b36965a60e6e9daf918a3a922f3f
emon273273 <emon.cse273@gmail.com>
2024-10-08 14:27:16 +0600

    created login api

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 // Import the controller
-const registerController = require("../controllers/authController");
+const {
+  registerController,
+  loginController,
+} = require("../controllers/authController");
 
 // Define the route
+
+//register post method
 router.post("/register", registerController);
 
+//login method
+router.post("/login", loginController);
 module.exports = router;
