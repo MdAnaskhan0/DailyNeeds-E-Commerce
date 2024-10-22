eca4f032e1a66ebba36ec39dc3145a0ddf2fff28
emon273273 <emon.cse273@gmail.com>
2024-10-22 11:07:28 +0600

    admin auth api created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -101,6 +101,7 @@ const loginController = (async = async (req, res) => {
         email: user.email,
         phone: user.phone,
         address: user.address,
+        role:user.role
       },
       token,
     });
